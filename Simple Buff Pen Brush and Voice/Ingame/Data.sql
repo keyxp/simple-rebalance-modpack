bef72b2e175afbaf49b5056f9aed5920c67e9657
keyxp <country_fam@naver.com>
2024-12-23 20:47:47 +0900

    Remove unused mods

--- Simple Buff Pen Brush and Voice/Ingame/Data.sql
+++
@@ -1,8 +0,0 @@
--- Do not encode UTF-8 BOM
-
-
-
--- Pen, Brush, and Voice
-
-UPDATE ModifierArguments SET Value = 2 WHERE Name = 'Amount' AND ModifierId IN
-('COMMEMORATION_CULTURAL_DISTRICTCULTURE');
